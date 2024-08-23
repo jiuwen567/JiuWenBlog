8b972d047b3ed944f4129a8aa3f42482650f47dc
九问 <2750826557@qq.com>
2024-08-23 13:22:07 +0800

    vitepress建站项目

---
+++ .vitepress/theme/index.js
@@ -0,0 +1,3 @@
+import Theme from '@escook/vitepress-theme'
+import '@escook/vitepress-theme/style.css'
+export default Theme   // https://www.escook.cn/index.php/2024/05/06/escook-vitepress-theme-doc/
